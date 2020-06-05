d0a0901f649bf0625ebfd19149628aa439df5774
Nazeh-Taha <nazeh3036@gmail.com>
2020-06-05 20:17:01 +0300

    add cart bage

--- frontend/src/Bages/CartBage.js
+++ frontend/src/Bages/CartBage.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react";
 import { addToCart } from "../actions/cartActions";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+
 
 function CartBage(props) {
-  const cart = useSelector(state=>state.cart);
-  const {cartItems} = cart;
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const dispatch = useDispatch();
+  const removeFromCartHandler = (productId) =>{
+    dispatch(removeFromCart(productId));
+ }
   const id = props.match.params.id;
   const qty = props.location.search
     ? Number(props.location.search.split("=")[1])
@@ -24,34 +31,43 @@ function CartBage(props) {
             <h3>Shoping List</h3>
             <div>Price</div>
           </li>
-          {cartItems.length === 0 ? 
-            (<div>Cart Is Empty</div>)
-           : 
-            cartItems.map(item => 
+          {cartItems.length === 0 ? (
+            <div>Cart Is Empty</div>
+          ) : (
+            cartItems.map((item) => (
               <>
-              <div>
-                <img src={item.image} alt="image" />
-                <div className="cart-name">
-                  <div>{item.name}</div>
-                </div>
-                <div>
-                  QTY:
-                  <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                  </select>
-                </div>
-              </div>
-              <div>
-                {item.price}
-              </div>
-            </>
+                <li>
+                  <div className="cart-image">
+                    <img src={item.image} alt="image" />
+                  </div>
+                  <div className="cart-name">
+                    <div>
+                      <Link to={"/products/" + item.product}>{item.name}</Link>
+                    </div>
+                    QTY:
+                    <select>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                    </select>
+                    <button type="button" onClick={()=>removeFromCartHandler(item.product)}>Delete</button>
+                  </div>
+
+                  <div className="cart-price">${item.price}</div>
+                </li>
+              </>
+            ))
           )}
         </ul>
       </div>
-      <div className="cart-action"></div>
+      <div className="cart-action">
+        <h3>
+          Subtotal ({cartItems.reduce((a, b) => a + b.qty, 0)} items) : $(
+          {cartItems.reduce((a, b) => a + b.price * b.qty, 0)})
+        </h3>
+        <button disabled={cartItems.length === 0}>Confirm Shoping</button>
+      </div>
     </div>
   );
 }
